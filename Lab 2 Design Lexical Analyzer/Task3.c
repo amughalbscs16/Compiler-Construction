d94040c757b9fb2976626c9885776f0b87d05c11
Ali Hassaan Mughal <amughal.bscs16seecs@seecs.edu.pk>
2019-09-12 16:17:50 +0500

    Identifiers and Operators seperated

--- Lab 2 Design Lexical Analyzer/Task3.c
+++ Lab 2 Design Lexical Analyzer/Task3.c
@@ -5,42 +5,104 @@
 Enter a string and differentiate the identifiers and operators
 */
 bool isOperator(char opr);
-bool isIdentifier(char identifier[]);
+bool isIdentifier(char identifier[],int,int);
 int main()
 {
+
+	int i,j,k,idenIndex=0,oprIndex = 0;
+	bool oprAlready=false;
+	char identifierList[5][20] = {"","","","",""};
+	char oprList[4];
+	char inputlexical[40];
+	int tmplastvar=0,linescount = 1;
+	printf("Enter the program: ");
+	scanf("%[^\n]s", inputlexical);
 	
+	for (i=0;inputlexical[i]!='\0';i++)
+	{
+		
+		if (inputlexical[i] == '\n')
+		linescount++;
+		if (isOperator(inputlexical[i]) || inputlexical[i] == ' ' || inputlexical[i] == '\t')
+		{
+			printf("operator %c \n",inputlexical[i]);
+			printf("starting var: %c %c \n", inputlexical[tmplastvar], inputlexical[i-1]);
+			if (isIdentifier(inputlexical,tmplastvar,i-1))
+			{
+				for (j=tmplastvar;j<=i-1;j++)
+				{
+					identifierList[idenIndex][j-tmplastvar] = inputlexical[j];
+				}
+				
+				printf("identifier %s\n", identifierList[idenIndex]);
+				idenIndex++;
+			}
+			tmplastvar = i+1;
+			//if operator already there don't add it
+			
+			for (k=0;k<4;k++)
+			{
+				if (inputlexical[i] == oprList[i])
+				{
+					oprAlready=true;
+				}
+			}
+		
+			if (!oprAlready)
+			{
+				oprList[oprIndex] = inputlexical[i];
+				oprIndex++;
+				oprAlready=false;
+			}
+		}
+		if (inputlexical[i+1] == '\0')
+		{
+			if (isIdentifier(inputlexical,tmplastvar,i))
+			{
+				for (j=tmplastvar;j<=i;j++)
+				{
+					identifierList[idenIndex][j-tmplastvar] = inputlexical[j];
+					//printf("%d %d", j-tmplastvar, j);
+				}
+				
+				printf("identifier %s\n", identifierList[idenIndex]);
+				idenIndex++;
+			}
+		}
+		
+	}
+	printf("%s\n", identifierList[2]);
+	printf("%s\n", inputlexical);
+	printf("The number of lines: %d", linescount);
 	return 0;
 }
+
 bool isOperator(char opr){
 	switch(opr){
 		case '*':
 		case '-':
 		case '+':
 		case '/':
-			printf("It is an operator");
 			return true;
 		default:
-			printf("It is not an operator");
 			return false;
 	}
 }
-bool isIdentifier(char identifier[]){
+
+bool isIdentifier(char identifier[],int firstvar,int lastvar){
 	int i;
 	//First - Ending Character
-	if (!(identifier[i] == '_' || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] >= 'a' && identifier[i] <= 'z')))
+	if (!(identifier[firstvar] == '_' || (identifier[firstvar] >= 'A' && identifier[firstvar] <= 'Z') || (identifier[firstvar] >= 'a' && identifier[firstvar] <= 'z')))
 	{
-		printf("%s: is an invalid identifier",identifier);
 		return false;
 	}
 	//Second - Ending Character
-	for (i=1;identifier[i]!='\0';i++)
+	for (i=firstvar+1;i<=lastvar;i++)
 	{
 		if (!(identifier[i] == '_' || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] >= 'a' && identifier[i] <= 'z') || (identifier[i] >= '0' && identifier[i] <= '9')))
 		{
-			printf("%s: is an invalid identifier",identifier);
 			return false;
 		}
 	}
-	printf("%s: is a valid identifier",identifier);
 	return true;
 }
